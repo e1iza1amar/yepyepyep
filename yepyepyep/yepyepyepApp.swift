6dd85243e54199ac1c2214ca311a7296125fa6d2
Eliza Lamar <elizalamar7@gmial.com>
2023-06-26 15:24:30 -0400

    Initial Commit

---
+++ yepyepyep/yepyepyepApp.swift
@@ -0,0 +1,17 @@
+//
+//  yepyepyepApp.swift
+//  yepyepyep
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct yepyepyepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
